b5c13a051ba94320cfb41dea39e73f39249010f6
luis10laso <luis.diez@gruposantander.com>
2023-05-03 15:53:22 +0200

    First commit

---
+++ src/main/java/com/luis/app/App.java
@@ -0,0 +1,13 @@
+package com.luis.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
